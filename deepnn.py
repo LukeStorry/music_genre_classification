8cced8292fd8a303165f35b8e163281ecae0c588
Matthew <matthewmkco@gmail.com>
2018-11-22 13:29:43 +0000

    layers

--- deepnn.py
+++ deepnn.py
@@ -4,12 +4,11 @@ import librosa
 import pickle
 import utils
 
-<<<<<<< HEAD
+
 def load_music():
     with open('music_genres_dataset.pkl', 'rb') as f:
         train_set = pickle.load(f)
         test_set = pickle.load(f)
     return train_set, test_set
-=======
+
 train_set, test_set = utils.load_music()
->>>>>>> 6a9fbcd9808f37b888fd68545db5eecb736d839f
