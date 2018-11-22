e4ad9a6133c90573c8cbf2cd78d7446ddf19a41c
Matthew <matthewmkco@gmail.com>
2018-11-22 12:23:22 +0000

    pickle

---
+++ shallownn.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+import pickle
+
+load_music():
+    with open('music_genres_dataset.pkl', 'rb') as f:
+        train_set = pickle.load(f)
+        test_set = pickle.load(f)
+    return train_set, test_set
