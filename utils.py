8598086cbb67cee0da861a06474ae2e799b890f7
Luke Storry <luke@lukestorry.co.uk>
2018-11-26 19:12:46 +0000

    temporary edits to utils for testing purposes

--- utils.py
+++ utils.py
@@ -4,14 +4,36 @@ import pickle
 
 
 def melspectrogram(audio):
-    spec = librosa.stft(audio, n_fft=512, window='hann', hop_length=256, win_length=512, pad_mode='constant')
+    spec = librosa.stft(audio, n_fft=512, window='hann',
+                        hop_length=256, win_length=512, pad_mode='constant')
     mel_basis = librosa.filters.mel(sr=22050, n_fft=512, n_mels=80)
     mel_spec = np.dot(mel_basis, np.abs(spec))
     return np.log(mel_spec + 1e-6)
 
+
 def load_music():
+    print("Loading data...")
     with open('music_genres_dataset.pkl', 'rb') as f:
+        train_set = test_set = {
+        'labels': [1,2,3,4,5,6],
+        'data' : [[11],  [22], [33], [44], [55], [66]],
+        'track_id': [1,1,1,1,2,2]
+        }
+
         train_set = pickle.load(f)
-        test_set = pickle.load(f)
-    print("Music Data Loaded")
+        # test_set = pickle.load(f)
+
+    print("Music Data Loaded.")
     return train_set, test_set
+
+# a,b = load_music()
+# audio = a['data'][0]
+# mel =  melspectrogram(audio)
+#
+# print type(audio)
+# print audio.shape
+# print audio
+#
+# print mel
+# print type(mel)
+# print mel.shape
