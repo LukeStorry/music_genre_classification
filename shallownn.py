59e2d6976a8b27f4a71a5c10b9e0e511536f5174
Sydney <sd14814@my.bristol.ac.uk>
2018-11-26 11:41:36 +0000

    Hyperparameter flags from the lab

--- shallownn.py
+++ shallownn.py
@@ -5,6 +5,34 @@ import utils
 
 train_set, test_set = utils.load_music()
 
+
+'''
+Hyperparameter flags taken from lab code.
+
+'''
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string('data_dir', os.getcwd() + '/dataset/',
+                            'Directory where the dataset will be stored and checkpoint. (default: %(default)s)')
+tf.app.flags.DEFINE_integer('max_steps', 10000,
+                            'Number of mini-batches to train on. (default: %(default)d)')
+tf.app.flags.DEFINE_integer('log_frequency', 10,
+                            'Number of steps between logging results to the console and saving summaries (default: %(default)d)')
+tf.app.flags.DEFINE_integer('save_model', 1000,
+                            'Number of steps between model saves (default: %(default)d)')
+
+# Optimisation hyperparameters
+tf.app.flags.DEFINE_integer('batch_size', 256, 'Number of examples per mini-batch (default: %(default)d)')
+tf.app.flags.DEFINE_float('learning_rate', 1e-5, 'Learning rate (default: %(default)d)')
+tf.app.flags.DEFINE_integer('img_width', 32, 'Image width (default: %(default)d)')
+tf.app.flags.DEFINE_integer('img_height', 32, 'Image height (default: %(default)d)')
+tf.app.flags.DEFINE_integer('img_channels', 3, 'Image channels (default: %(default)d)')
+tf.app.flags.DEFINE_integer('num_classes', 10, 'Number of classes (default: %(default)d)')
+tf.app.flags.DEFINE_string('log_dir', '{cwd}/logs/'.format(cwd=os.getcwd()),'Directory where to write event logs and checkpoint. (default: %(default)s)')
+
+
+
 xavier_initializer = tf.contrib.layers.xavier_initializer(uniform=True)
 
 def shallownn(x_images):
