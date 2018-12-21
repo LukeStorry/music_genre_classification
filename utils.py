3cc364b8934feab45c81318c7087c274c9700e9a
Luke Storry <luke@lukestorry.co.uk>
2018-12-21 00:08:53 +0000

    added augmentation functions to utils

--- utils.py
+++ utils.py
@@ -8,3 +8,13 @@ def melspectrogram(audio):
     mel_basis = librosa.filters.mel(sr=22050, n_fft=512, n_mels=80)
     mel_spec = np.dot(mel_basis, np.abs(spec))
     return np.log(mel_spec + 1e-6)
+
+def time_stretch(sample, rate):
+    stretched = librosa.effects.time_stretch(sample, rate)
+    if len(stretched) >= 20462:
+        return stretched[:20462]
+    else:
+        return np.pad(stretched, (0, 20462-len(stretched)), 'constant')
+
+def pitch_shift(sample, half_steps):
+    return librosa.effects.pitch_shift(sample, 22050, half_steps)
